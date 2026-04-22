6b8cc04cb1b7af0753023d05520dc418cf976cfe
balajasa <balajasasa@gmail.com>
2026-04-22 08:58:35 +0800

    hotfix login

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from '@/firebase'
 
 const router = createRouter({
@@ -7,6 +8,7 @@ const router = createRouter({
     {
       path: '/',
       component: () => import('@/layouts/DefaultLayout.vue'),
+      meta: { requiresAuth: true },
       children: [
         { path: '', redirect: '/collection' },
         {
@@ -35,8 +37,23 @@ const router = createRouter({
   ],
 })
 
-router.beforeEach((to) => {
-  if (to.name === 'Login' && auth.currentUser) {
+function getCurrentUser() {
+  return new Promise((resolve) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      unsubscribe()
+      resolve(user)
+    })
+  })
+}
+
+router.beforeEach(async (to) => {
+  const user = await getCurrentUser()
+
+  if (to.matched.some((r) => r.meta.requiresAuth) && !user) {
+    return { name: 'Login' }
+  }
+
+  if (to.name === 'Login' && user) {
     return { name: 'Collection' }
   }
 })
